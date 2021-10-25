e721247c568e7e13bd069b21db36124baef83374
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-10-25 16:24:18 -0500

    methods of type strings

---
+++ strings.py
@@ -0,0 +1,28 @@
+my_str = "Hello World"
+
+print(dir(my_str))
+
+print("Saludar: " + my_str)
+print(f"Saludar: {my_str}")
+
+print(my_str)
+
+print(my_str.upper())
+print(my_str.lower())
+print(my_str.swapcase())
+print(my_str.capitalize())
+print(my_str.title())
+
+print(my_str.replace("Hello", "Yuli"))
+
+print(my_str.startswith("H"))
+print(my_str.endswith("a"))
+print(my_str.isnumeric())
+print(my_str.isalpha())
+
+print(my_str.split())
+
+print(my_str.find("o"))
+print(len(my_str))
+print(my_str.index("e"))
+print(my_str.count("l"))
