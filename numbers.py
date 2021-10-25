da4f387df0c097bc2880e769d6b4d521ede55bf6
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-10-25 16:49:14 -0500

    Numbers

---
+++ numbers.py
@@ -0,0 +1,12 @@
+print(1 - 2)
+print(1 + 2)
+print(2 * 2)
+print(3 / 2)
+print(3 // 2)
+print(3 % 2)
+print(1 + 1.0)
+print(2 ** 2)
+
+age = input("Insert your age: ")
+new_age = int(age) + 5
+print(new_age)
