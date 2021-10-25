6fa170cb17c496571c69b74b58b50f7dcb5ff561
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-10-25 13:29:09 -0500

    Type string

---
+++ data_type.py
@@ -0,0 +1,11 @@
+# string
+print("Hello world")
+print("Hello world")
+print("""Hello world""")
+print("""Hello world""")
+
+# para ver el tipo de dato
+print(type("Hello world"))
+
+# concatenar
+print("Bye" + " " + "world")
