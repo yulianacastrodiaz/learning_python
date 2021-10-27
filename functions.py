bf95b001a5be76f7deed10d4df59ae0a08b3c866
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-10-27 13:41:40 -0500

    functions

---
+++ functions.py
@@ -0,0 +1,17 @@
+def saludar(saludo="Hola Mundo"):
+    print(saludo)
+
+
+saludar("holitassssssssss")
+saludar("anfhkersnfierw")
+saludar()
+
+
+def suma(n1, n2):
+    return n1 + n2
+
+
+print(suma(10, 456))
+
+suma2 = lambda n1, n2: n1 + n2
+print(suma2(20, 40))
