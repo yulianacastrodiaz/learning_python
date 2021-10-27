f0f7bb9bfb3694124b4f02d769416cd5f3f7ae99
yulianacastro7 <yulianacastrodiaz7@gmail.com>
2021-10-27 18:34:16 -0500

    List intermediate

--- lists.py
+++ lists.py
@@ -47,4 +47,40 @@ print(demo_list)
 print(min(numbers_list))
 print(max(numbers_list))
 
-help(numbers_list)
+# help(numbers_list)
+
+new_list = [1] * 10
+print(new_list)
+
+new_list2 = new_list  # pasaje por referencia
+print(new_list2)
+
+new_list2[7] = 3
+print(new_list)
+print(new_list2)
+
+copy_of_list = new_list.copy()  # pasaje por valor
+copy_of_list[3] = 4
+print(new_list)
+print(copy_of_list)
+
+new_list = [1] * 10
+list_slice = new_list[:]  # asigna todos los valores de new_list y es pasaje por valor
+list_slice[2] = 34
+print(new_list)
+print(list_slice)
+
+print(list_slice[3:])  # desde el índice 3 hasta el largo
+print(list_slice[:5])  # desde el comienzo hasta el índice 5
+print(list_slice[3:5])
+print(list_slice[3:7:2])  # desde la posición 3 hasta la 7 de 2 en 2
+
+list_with_for = [i for i in range(10)]
+list_with_for2 = [i ** 2 for i in range(10)]
+print(list_with_for)
+print(list_with_for2)
+
+matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(matriz)
+print(matriz[0][2])
+print(matriz[2][1])
